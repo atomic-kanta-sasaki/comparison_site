8cc24b30bb872058f220bebaa268ba95742b2f35
kanta_sasaki <k_padbjw@icloud.com>
2023-06-03 21:05:30 +0900

    error handling and login

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -1,1 +1,44 @@
+import { PrismaClient, User } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { UserBaseRequestBody } from '../interfaces/user'
+import { CustomErrorHandler } from '../middleware/error'
+const prisma = new PrismaClient();
 
+
+
+export const registerUser = async (body: UserBaseRequestBody) => {
+  const { email, password } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user: User | null = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return user;
+}
+
+export const login = async (body: UserBaseRequestBody) => {
+  const { email, password } = body;
+  const user: User | null = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    throw new CustomErrorHandler("user not found", 404);
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new CustomErrorHandler("Invalid passowrd", 400);
+  }
+  const token = jwt.sign({ userId: user.id }, 'your_secret_key');
+
+  return token;
+}
--- api/middleware/auth.ts
+++ api/middleware/auth.ts
@@ -1,15 +1,21 @@
 import jwt from 'jsonwebtoken'
+import express from 'express'
 
-export const authenticateToken = (req, res, next) => {
-  // Authorization: 'Bearer TOKEN'
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
 
-  if (token == null) return res.sendStatus(401); // if there isn't any token
+export const authenticate = (req: any, res: express.Response, next: express.NextFunction) => {
+  const authHeader = req.headers.authorization;
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const payload = jwt.verify(token, 'your_secret_key') as jwt.JwtPayload;
+    req.userId = payload.userId;
     next();
-  });
-}
+  } catch (e) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+};
